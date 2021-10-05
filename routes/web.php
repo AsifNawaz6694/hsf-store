95878229c8fd49bc173923c8d8d8f03e6079a2da
AsifNawaz6694 <asif@bargoventures.com>
2021-10-05 17:40:26 +0500

    register route closed

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::middleware(['setData'])->group(function () {
 
     Auth::routes();
 
-    Route::get('/business/register', 'BusinessController@getRegister')->name('business.getRegister');
+//    Route::get('/business/register', 'BusinessController@getRegister')->name('business.getRegister');
     Route::post('/business/register', 'BusinessController@postRegister')->name('business.postRegister');
     Route::post('/business/register/check-username', 'BusinessController@postCheckUsername')->name('business.postCheckUsername');
     Route::post('/business/register/check-email', 'BusinessController@postCheckEmail')->name('business.postCheckEmail');
