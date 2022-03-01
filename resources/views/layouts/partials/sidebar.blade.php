0b1e3e5d833a9a5d2d34b1390fbc29b00ec8ec92
AsifNawaz6694 <asif@bargoventures.com>
2022-03-01 16:31:07 +0500

    check v.1

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -5,7 +5,7 @@
   <section class="sidebar">
 
 	<a href="{{route('home')}}" class="logo">
-		<span class="logo-lg">asdas</span>
+		<span class="logo-lg">{{ Session::get('business.name') }}</span>
 	</a>
 
     <!-- Sidebar Menu -->
