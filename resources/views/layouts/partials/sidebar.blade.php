64d4b8bf6668cf1465c6097f91cefb5fe823a3d5
AsifNawaz6694 <asif@bargoventures.com>
2022-03-01 16:33:21 +0500

    check v.12

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,17 +1,17 @@
 <!-- Left side column. contains the logo and sidebar -->
 <aside class="main-sidebar">
 
-  <!-- sidebar: style can be found in sidebar.less -->
-  <section class="sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
 
-	<a href="{{route('home')}}" class="logo">
-		<span class="logo-lg">{{ Session::get('business.name') }}</span>
-	</a>
+        <a href="{{route('home')}}" class="logo">
+            <span class="logo-lg">{{ Session::get('business.name') }}</span>
+        </a>
 
-    <!-- Sidebar Menu -->
+        <!-- Sidebar Menu -->
     {!! Menu::render('adminltecustom', 'admin-sidebar-menu'); !!}
 
     <!-- /.sidebar-menu -->
-  </section>
-  <!-- /.sidebar -->
+    </section>
+    <!-- /.sidebar -->
 </aside>
