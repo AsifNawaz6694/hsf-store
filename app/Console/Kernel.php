5d264f48dd758588069ed8d2d0a7a99fa29d7fd7
AsifNawaz6694 <asif@bargoventures.com>
2022-09-23 17:20:08 +0500

    db backup process Cron daily v.4

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         $env = config('app.env');
         $email = config('mail.username');
-        $schedule->command('db:dbbackup')->everyMinute();
+        $schedule->command('create:dbbackup')->everyMinute();
 
         if ($env === 'live') {
             //Scheduling backup, specify the time when the backup will get cleaned & time when it will run.
