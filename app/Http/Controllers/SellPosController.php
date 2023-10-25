e2e7bb1fbb12f739608b031751565342afd73e8a
AsifNawaz6694 <asif@bargoventures.com>
2023-10-25 16:43:57 +0500

    check removed v.15

--- app/Http/Controllers/SellPosController.php
+++ app/Http/Controllers/SellPosController.php
@@ -941,9 +941,8 @@ class SellPosController extends Controller
         }
         
         try {
-            dd($request);
             $input = $request->except('_token');
-
+dd($input);
             //status is send as quotation from edit sales screen.
             $input['is_quotation'] = 0;
             if ($input['status'] == 'quotation') {
